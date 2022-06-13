518aaa9ac42ee7d496e56867aaca03d52da51806
TheMisterSenpai <kirasimshow@gmail.com>
2022-06-13 20:22:56 +0300

    :cowboy:

--- src/config/setting.py
+++ src/config/setting.py
@@ -4,13 +4,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 token = os.environ.get("TOKEN")
+
 debug = False
 version = "3.0.0.dev1"
 prefix = "m"
 devprefix = "m#"
+color = 0x2f3136
 
 
 plugins = [
-    "plugins.ping"
+    "plugins.ping",
+    "plugins.eval"
 ]
 
