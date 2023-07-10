8eea6c74303b35318401780b2a64937e9afcd664
TheMisterSenpai <kirasimshow@gmail.com>
2023-07-10 17:10:58 +0300

    fix reaction

--- src/plugins/reaction/api_reaction.py
+++ src/plugins/reaction/api_reaction.py
@@ -1,19 +1,9 @@
 import requests
-from pynepcord.base import ImageSession
-import random
-
-from config import setting
-
 
 def nekos_best(reaction):
     img = requests.get(f"https://nekos.best/api/v2/{reaction}").json()
     return img["results"][0]["url"]
 
-def pynepcord(reaction):
-    session = ImageSession(setting.pynepcord_token)
-    image = session.get_image(reaction)
-    return image.url
-
 def img_api(reaction):
-    image = random.choice([nekos_best(reaction), pynepcord(reaction)])
+    image = nekos_best(reaction)
     return image
