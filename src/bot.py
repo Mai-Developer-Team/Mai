b65a5aa93bff95667bb3869606212b6c8692a493
ths_dev <61903983+TheMisterSenpai@users.noreply.github.com>
2023-09-11 09:28:00 +0300

    test status

--- src/bot.py
+++ src/bot.py
@@ -42,5 +42,5 @@ if __name__ == '__main__':
 client.run(
     activity = hikari.Activity( 
         name = setting.status,
-        type = hikari.ActivityType.COMPETING)
+        type = hikari.ActivityType.CUSTOM)
     )
